9fc58f8d3c1a2144c5b7f1f12f20890173591b5e
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-09-24 13:42:31 +0200

    feat: add commitlint and editorconfig

--- migrations/2_mTKN_migration.js
+++ migrations/2_mTKN_migration.js
@@ -1,5 +1,5 @@
 const mTKNExample = artifacts.require("mTKNExample");
 
 module.exports = function(deployer) {
-  deployer.deploy(mTKNExample, "test meta token", "mTKNE", 18);
+    deployer.deploy(mTKNExample, "test meta token", "mTKNE", 18);
 };
